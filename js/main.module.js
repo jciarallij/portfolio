ee0ec65b46c8633aac4e126c6320ae00219b6ff7
jciarallij <jciarallij@gmail.com>
2018-05-02 10:04:29 -0400

    bridal test

--- js/main.module.js
+++ js/main.module.js
@@ -1,105 +1,107 @@
-var portfolioApp = angular.module('portfolioApp', ['ngRoute']);
+var portfolioApp = angular.module("portfolioApp", ["ngRoute"]);
 
-portfolioApp.directive('checkImage', function($http) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            attrs.$observe('ngSrc', function(ngSrc) {
-                $http.get(ngSrc).success(function() {
-                    // alert('image exist');
-                }).error(function() {
-                    // alert('image not exist');
-                    element.attr('src', 'img/placeholder2.jpg'); // set default image
-                });
-            });
-        }
-    };
+portfolioApp.directive("checkImage", function($http) {
+  return {
+    restrict: "A",
+    link: function(scope, element, attrs) {
+      attrs.$observe("ngSrc", function(ngSrc) {
+        $http
+          .get(ngSrc)
+          .success(function() {
+            // alert('image exist');
+          })
+          .error(function() {
+            // alert('image not exist');
+            element.attr("src", "img/placeholder2.jpg"); // set default image
+          });
+      });
+    }
+  };
 });
 
-
-
 portfolioApp.config(function($routeProvider) {
-    $routeProvider.when('/', {
-        templateUrl: 'views/main.html'
-    }).otherwise({
-        redirectTo: '/'
+  $routeProvider
+    .when("/", {
+      templateUrl: "views/main.html"
+    })
+    .otherwise({
+      redirectTo: "/"
     });
 });
 
+portfolioApp.controller("mainController", function(
+  $scope,
+  $http,
+  $routeParams,
+  $interval,
+  $location
+) {
+  console.log("Ciaralli.com Controller has loaded!");
+  $scope.name = "";
 
-portfolioApp.controller('mainController', function($scope, $http, $routeParams, $interval, $location) {
+  $scope.ticket = false;
+  $scope.projectTicket = true;
 
-    console.log("Ciaralli.com Controller has loaded!");
-    $scope.name = "";
+  $scope.twitter = false;
+  $scope.projectTwitter = true;
 
-    $scope.ticket = false;
-    $scope.projectTicket = true;
+  $scope.blackjack = false;
+  $scope.projectBlackjack = true;
 
-    $scope.twitter = false;
-    $scope.projectTwitter = true;
+  $scope.dollar = false;
+  $scope.projectDollar = true;
 
-    $scope.blackjack = false;
-    $scope.projectBlackjack = true;
+  $scope.mygrub = false;
+  $scope.projectMygrub = true;
 
-    $scope.dollar = false;
-    $scope.projectDollar = true;
+  $scope.roast = false;
+  $scope.projectRoast = true;
 
-    $scope.mygrub = false;
-    $scope.projectMygrub = true;
+  $scope.emailFunc = function() {
+    $http({
+      method: "post",
+      url: "mail.php",
+      dataType: "json",
+      data: {
+        name: $scope.name,
+        emailAddr: $scope.emailAddr,
+        emailSubject: $scope.emailSubject,
+        emailBody: $scope.emailBody
+      }
+    }).then(
+      function(success) {
+        console.log(success);
+        $scope.emailAddr = "";
+        $scope.emailBody = "";
+        $scope.emailSubject = "";
+        $scope.name = "";
+        console.log("Email Sent!");
+      },
+      function(error) {
+        console.log(error);
+      }
+    );
+  };
 
-    $scope.roast = false;
-    $scope.projectRoast = true;
-
-
-    $scope.emailFunc = function() {
-        $http({
-            method: "post",
-            url: "mail.php",
-            dataType: 'json',
-            data: {
-                name: $scope.name,
-                emailAddr: $scope.emailAddr,
-                emailSubject: $scope.emailSubject,
-                emailBody: $scope.emailBody
-            }
-
-        }).then(
-            function(success) {
-                console.log(success);
-                $scope.emailAddr = "";
-                $scope.emailBody = "";
-                $scope.emailSubject = "";
-                $scope.name = "";
-                console.log("Email Sent!");
-            },
-            function(error) {
-                console.log(error);
-            });
-    };
-
-
-    $scope.showProject = function(project) {
-        if (project === 1) {
-            $scope.ticket = true;
-            $scope.projectTicket = false;
-        } else if (project === 2) {
-            $scope.twitter = true;
-            $scope.projectTwitter = false;
-        } else if (project === 3) {
-            $scope.blackjack = true;
-            $scope.projectBlackjack = false;
-        } else if (project === 4) {
-            $scope.dollar = true;
-            $scope.projectDollar = false;
-        } else if (project === 5) {
-            $scope.mygrub = true;
-            $scope.projectMygrub = false;
-        } else if (project === 6) {
-            $scope.roast = true;
-            $scope.projectRoast = false;
-        }
-
-
-    };
-
+  $scope.showProject = function(project) {
+    if (project === 1) {
+      $scope.ticket = true;
+      $scope.projectTicket = false;
+    } else if (project === 2) {
+      $scope.twitter = true;
+      $scope.projectTwitter = false;
+    } else if (project === 3) {
+      $scope.blackjack = true;
+      $scope.projectBlackjack = false;
+    } else if (project === 4) {
+      $scope.dollar = true;
+      $scope.projectDollar = false;
+    } else if (project === 5) {
+      $scope.mygrub = true;
+      $scope.projectMygrub = false;
+    } else if (project === 6) {
+      $scope.roast = true;
+      $scope.projectRoast = false;
+    }
+  };
 });
